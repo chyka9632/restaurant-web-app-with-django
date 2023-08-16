44dd9e40cb65e17259832442332a8ecc4420ca52
Chika <chyka9632@gmail.com>
2023-08-16 12:08:23 +0100

    Context in django - sending data from python to your html

--- restaurant_menu/views.py
+++ restaurant_menu/views.py
@@ -7,7 +7,12 @@ from restaurant_menu.models import Item
 class MenuList(generic.ListView):    # Class-based view
     queryset = Item.objects.order_by("date_created")
     template_name = "index.html"
-    pass
+
+    def get_context_data(self):
+        context = {"meals": ["Pizza", "Pasta"],
+                   "ingredients": ["things"]}
+        return context
+
 
 
 class MenuItemDetails(generic.DetailView):
